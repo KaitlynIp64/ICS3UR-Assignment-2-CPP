fc10e4422eb909fd4a7cf05ba0883ade87f0908d
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-10-06 15:01:50 +0000

    added code

---
+++ areaAndPerimeterOfEquilateralTriangle.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2022 Kaitlyn Ip All rights reserved
+//
+// Created by: Kaitlyn Ip
+// Created on: Oct 2022
+// This program calculates the area of perimeter of an equilateral triangle
+//    with side inputted from the user
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    // this function calculates area and perimeter of equilateral triangle
+    int side;
+    double area;
+    double perimeter;
+    // input
+    std::cout << "Enter the side of the equilateral triangle (mm): ";
+    std::cin >> side;
+    // process
+    area = sqrt(3) / 4 * (pow(side, 2));
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << area << " mm²" << std::endl;
+    std::cout << "Perimeter is " << perimeter << " mm" << std::endl;
+    std::cout << "\nDone." << std::endl;
+}
